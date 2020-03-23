dbb66ccf690b1c554e84128597dd6afe953dc661
haihua.gu <haihua.gu@gaiaworks.cn>
2020-03-23 16:03:42 +0800

    update

--- src/main/java/com/ghh/sample/model/vo/ResponseData.java
+++ src/main/java/com/ghh/sample/model/vo/ResponseData.java
@@ -1,14 +1,37 @@
 package com.ghh.sample.model.vo;
 
+import com.fasterxml.jackson.annotation.JsonInclude;
+import com.github.pagehelper.Page;
+
+import java.util.ArrayList;
+
 public class ResponseData {
     private int code;
     private String message;
     private Object data;
 
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer pageNum;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer pageSize;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Long total;
+    @JsonInclude(JsonInclude.Include.NON_NULL)
+    private Integer pages;
+
     public ResponseData(int code, String message, Object data) {
         this.code = code;
         this.message = message;
-        this.data = data;
+        if (data instanceof Page) {
+            Page<?> pagedData = (Page<?>)data;
+            this.pageNum = pagedData.getPageNum();
+            this.pageSize = pagedData.getPageSize();
+            this.total = pagedData.getTotal();
+            this.pages = pagedData.getPages();
+            this.data = new ArrayList<>(pagedData);
+        } else {
+            this.data = data;
+        }
     }
 
     public static ResponseData ok() {
@@ -49,5 +72,37 @@ public class ResponseData {
 
     public void setData(Object data) {
         this.data = data;
+    }
+
+    public Integer getPageNum() {
+        return pageNum;
+    }
+
+    public void setPageNum(Integer pageNum) {
+        this.pageNum = pageNum;
+    }
+
+    public Integer getPageSize() {
+        return pageSize;
+    }
+
+    public void setPageSize(Integer pageSize) {
+        this.pageSize = pageSize;
+    }
+
+    public Long getTotal() {
+        return total;
+    }
+
+    public void setTotal(Long total) {
+        this.total = total;
+    }
+
+    public Integer getPages() {
+        return pages;
+    }
+
+    public void setPages(Integer pages) {
+        this.pages = pages;
     }
 }
