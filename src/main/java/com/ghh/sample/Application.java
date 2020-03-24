3959b6c632f50879cbac9a88cfd3488e391d5983
haihua.gu <haihua.gu@gaiaworks.cn>
2020-03-24 13:04:10 +0800

    update

--- src/main/java/com/ghh/sample/Application.java
+++ src/main/java/com/ghh/sample/Application.java
@@ -4,9 +4,13 @@ package com.ghh.sample;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.ghh.sample.mapper")
+@ComponentScan(basePackages = {"com.ghh.sample.config",
+		"com.ghh.sample.controller",
+		"com.ghh.sample.service"})
 public class Application {
 
 	public static void main(String[] args) {
