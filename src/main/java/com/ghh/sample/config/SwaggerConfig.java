789bd55e86ce7f3eb80a03072c8b6f018d9375ee
haihua.gu <haihua.gu@gaiaworks.cn>
2020-03-27 13:49:13 +0800

    update

--- src/main/java/com/ghh/sample/config/SwaggerConfig.java
+++ src/main/java/com/ghh/sample/config/SwaggerConfig.java
@@ -1,11 +1,12 @@
 package com.ghh.sample.config;
 
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -23,6 +24,14 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build().apiInfo(apiInfo());
+    }
+
+    private ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("项目名")
+                .description("项目描述")
+                .version("1.0")
                 .build();
     }
 }
