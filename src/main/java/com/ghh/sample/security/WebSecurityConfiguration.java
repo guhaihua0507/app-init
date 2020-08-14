bb304367dffe89d024038050a9a095f1b2304a7a
haihua.gu <haihua.gu@gaiaworks.cn>
2020-08-14 17:10:15 +0800

    update

--- src/main/java/com/ghh/sample/security/WebSecurityConfiguration.java
+++ src/main/java/com/ghh/sample/security/WebSecurityConfiguration.java
@@ -7,6 +7,7 @@ import org.springframework.security.config.annotation.authentication.builders.Au
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
@@ -23,6 +24,7 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
         http.authorizeRequests()
                 .antMatchers("/**/login").permitAll()
                 .anyRequest().authenticated()
+//            .and().sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS) //如果使用JWT, 则不需要session
             .and().formLogin()
             .and().logout().permitAll()
             .and().csrf().disable();
